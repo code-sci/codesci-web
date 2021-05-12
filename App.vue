f4c39159e42404e63c00b00f3e5c4b7444171a82
codesci <codesci@126.com>
2021-05-12 23:37:00 +0800

    init

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	@font-face {
+	  font-family: 'iconfont';
+	  src: url('./static/imags/iconfont/iconfont.woff2') format('woff2'),
+	       url('./static/imags/iconfont/iconfont.woff?t=1620832462463') format('woff'),
+	       url('./static/imags/iconfont/iconfont.ttf?t=1620832462463') format('truetype');
+	}
+	/*每个页面公共css */
+</style>
